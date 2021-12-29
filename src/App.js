93d73fbf62937a42b96a9d167544e87dd7b3680f
Nissan Dookeran <nissan.dookeran@gmail.com>
2021-12-29 20:52:45 +1000

    upgrade react version, customise homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import * as React from "react";
+import React, {useEffect} from "react";
 import { ethers } from "ethers";
 import './App.css';
-
-export default function App() {
+const App = () => {
 
   const wave = () => {
     
@@ -17,7 +16,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+        I am Nissan and I am learning to code my first DApp so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
         <button className="waveButton" onClick={wave}>
@@ -27,3 +26,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App
